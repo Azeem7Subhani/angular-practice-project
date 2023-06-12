5050bf17ad6228d5e2295276bae89e5207cbe1ec
Azeem Subhani <asubhani@cinnova.com>
2023-06-12 15:37:09 +0500

    Making the require commit.
     - Project Setup
     - Components Created

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.modal';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Boiled Vegs', 'Fresh Vegitables for the healty body', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80')
+  ];
+}
